f2fef4429c4e416e07bfa9641dbbe930eaa68a5f
Sumanbitw <007sumanghosh@gmail.com>
2020-12-15 00:54:44 +0530

    added minions translation

--- App.js
+++ App.js
@@ -3,10 +3,28 @@ var txtInput = document.querySelector("#text-input")
 var outputDiv = document.querySelector("#output-box")
 
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json" 
 
+function getTranslatedURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Something has gone wrong. Try Again Later")
+}
 
 function clickEventHandler(){
-    outputDiv.innerText = "aaghriojfoijfoiihaaa" + txtInput.value
-}
+    var inputText = txtInput.value
+
+    fetch(getTranslatedURL(inputText))
+        .then(response=>response.json())
+        .then(json =>{
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+    
+};
 
 btnTranslate.addEventListener("click", clickEventHandler)
